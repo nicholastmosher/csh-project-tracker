e24065dedbdcecef0e3a24a924f5c7c4273763a1
Nick Mosher <nicholastmosher@gmail.com>
2019-09-21 21:29:02 -0400

    Configure hot reloading for actix

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,4 @@
+use actix_web::{App, web, Responder, HttpServer};
 use diesel::prelude::*;
 
 use csh_project_tracker::*;
@@ -16,4 +17,34 @@ fn main() {
     for project in results {
         println!("{:?}", project);
     }
+
+    http_server();
+}
+
+fn index() -> impl Responder {
+    "Hello, what the shit!"
+}
+
+fn get_projects() -> impl Responder {
+
+}
+
+fn http_server() -> std::io::Result<()> {
+    let mut listenfd = listenfd::ListenFd::from_env();
+
+    let mut server = HttpServer::new(||
+        App::new().service(
+            web::scope("/api/v1")
+                .route("/index.html", web::get().to(index))
+                .route("/projects", web::get().to(get_projects)))
+    );
+
+    server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
+        server.listen(l)?
+    } else {
+        server.bind("127.0.0.1:3000")?
+    };
+
+    server.run().unwrap();
+    Ok(())
 }
